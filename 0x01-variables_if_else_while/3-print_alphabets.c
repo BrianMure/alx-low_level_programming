7bd8478de2eb39799419134b8a814407302fa4bf
BrianMure <b.mureithi40@gmail.com>
2022-03-11 06:38:50 -0800

    Task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,11 @@
  */
 int main(void)
 {
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
+char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	putchar(ch);
 	ch = 'A'
-	for (ch = 'A'; ch <= 'Z' ch++)
+	for (ch = 'A'; ch <= 'Z' ; ch++)
 	putchar(ch);
 	putchar('\n')
 	return(0);

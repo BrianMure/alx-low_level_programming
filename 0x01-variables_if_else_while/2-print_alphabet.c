ecbe76bad9bf2bc587cd707a201234547e09e735
BrianMure <b.mureithi40@gmail.com>
2022-03-11 06:08:36 -0800

    Task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,18 +7,12 @@
  */
 int main(void)
 {
-	int num;
-	num = 97;
-	while (num <= 122)
-		putchar(num);
-	num++;
-	int p;
-	p = 97;
-	while (p <= 122)
+	char c = 'a';
+	while (c <= 'z')
 	{
-		putchar(p);
-		p++;
+		putchar(c);
+		c++
 	}
-	putchar( '\n');
-	return(0);
+	putchar( '\n' );
+	return (0);
 }

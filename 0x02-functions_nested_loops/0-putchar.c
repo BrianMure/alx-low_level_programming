8c7b3f05ed15715f4a13ed9068ac03de1b0343a4
BrianMure <b.mureithi40@gmail.com>
2022-03-16 05:06:34 -0700

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,20 @@
-/* Prototype of _putchar function */
-int _putchar(char c);
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
 
-/* prototype of print_putchar function */
-void print_putchar(void);
+int main(void)
+{
+	char c[10] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}

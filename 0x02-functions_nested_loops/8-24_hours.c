020c386d8ae4da0d8b84fbf9b024b2e12d4c2e52
BrianMure <b.mureithi40@gmail.com>
2022-03-16 09:23:31 -0700

    Task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,17 +5,17 @@
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int H, M;
 
-	for (i = 0; i < 24; i++)
+	for (H = 0; H < 24; H++)
 	{
-		for (j = 0; j < 60; j++)
+		for (M = 0; M < 60; M++)
 		{
-			_putchar(i / 10 + 48);
-			_putchar(i % 10 + 48);
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
 			_putchar(':');
-			_putchar(j / 10 + 48);
-			_putchar(j % 10 + 48);
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
 			_putchar('\n');
 		}
 	}

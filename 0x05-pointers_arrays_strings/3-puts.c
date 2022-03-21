e46be5e5431b0913aeb42f827cb5ed3dcf7b76da
BrianMure <b.mureithi40@gmail.com>
2022-03-21 06:34:51 -0700

    Task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * followed by a new line
+ * @str: input string
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+
+		_putchar(*str++);
+
+	_putchar('\n');
+}

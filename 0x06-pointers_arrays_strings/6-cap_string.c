beb04edcbd019a7ba6cb301008082dfb862f46f7
BrianMure <b.mureithi40@gmail.com>
2022-03-24 04:36:20 -0700

    capitalizes lowercase

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,62 @@
 #include "main.h"
 
+
+
 /**
- * cap_string - capitalizes all letters
- * @s: input string
- * Return: the pointer to dest
- */
+ *
+ *  * cap_string - function that capitalizes all words of a string.
+ *
+ *   * @s: Pointer to char
+ *
+ *    * Return: char
+ *
+ *     */
 
 char *cap_string(char *s)
-{
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41,123, 125}
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+{
+
+	int x = 0;
+
+
+
+	while (*(s + x) != '\0')
+
 	{
-		for (i = 0; i < 13; i++)
+
+		if (x == 0 && (*(s + x) >= 97 && *(s + x) <= 122))
+
 		{
-			if (*(s + count) == sep_words[i])
+
+			*(s + x) = *(s + x) - ' ';
+
+			x++;
+
+		}
+
+		if (*(s + x) == ' ' || *(s + x) == '\n' || *(s + x) == '\t' || *(s + x) == ',' || *(s + x) == ';' || *(s + x) == '!' || *(s + x) == '?' || *(s + x) == '"' || *(s + x) == '(' || *(s + x) == ')' || *(s + x) == '{' || *(s + x) == '}' || *(s + x) == '.')
+
+		{
+
+			x++;
+
+			if (*(s + x) >= 97 && *(s + x) <= 122)
+
 			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
+
+				*(s + x) = *(s + x) - ' ';
+
 			}
 
 		}
-		count++;
+
+		else
+
+			x++;
+
 	}
+
 	return (s);
+
 }
+

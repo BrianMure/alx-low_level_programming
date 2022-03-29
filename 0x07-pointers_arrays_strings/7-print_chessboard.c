5af02301cae6acbf82d55bbb88cb0bb87dac3fa0
BrianMure <b.mureithi40@gmail.com>
2022-03-28 21:26:58 -0700

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -17,13 +17,31 @@
 void print_chessboard(char (*a)[8])
 
 {
-int m, n;
-for (m = 0; m < 8; m++)
-{
-for (n = 0; n < 8; n++)
-{
-_putchar (*(*(a + m) + n));
-}
-_putchar ('\n');
-}
+
+
+
+	int m, n;
+
+
+
+	for (m = 0; m < 8; m++)
+
+	{
+
+		for (n = 0; n < 8; n++)
+
+		{
+
+			_putchar (*(*(a + m) + n));
+
+		}
+
+
+
+		_putchar ('\n');
+
+	}
+
+
+
 }
